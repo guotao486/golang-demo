ffbc7768cc1b0526692f310b8f860e4447119c1c
guotao <793954500@qq.com>
2022-07-25 17:17:02 +0800

    defer关键字

---
+++ demo_defer.go
@@ -0,0 +1,25 @@
+/*
+ * @Author: GG
+ * @Date: 2022-07-25 17:12:24
+ * @LastEditTime: 2022-07-25 17:16:30
+ * @LastEditors: GG
+ * @Description:
+ * @FilePath: \golang-demo\demo_defer.go
+ *
+ */
+package main
+
+import "fmt"
+
+func defer_f() {
+	fmt.Println("defer end....")
+}
+func main() {
+	fmt.Println("start....")
+
+	defer defer_f()
+	fmt.Println("step1")
+	fmt.Println("step2")
+	fmt.Println("step3")
+	fmt.Println("end..")
+}

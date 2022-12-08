c96d47f2734dc950d52939a6de5b463429adce23
793954500@qq.com <793954500@qq.com>
2022-12-08 15:05:09 +0800

    docs: 适配器模式

--- 设计模式/适配器模式/main.go
+++ 设计模式/适配器模式/main.go
@@ -1,7 +1,7 @@
 /*
  * @Author: GG
  * @Date: 2022-12-08 14:29:24
- * @LastEditTime: 2022-12-08 14:34:07
+ * @LastEditTime: 2022-12-08 15:04:42
  * @LastEditors: GG
  * @Description: 适配器模式
  * @FilePath: \设计模式\适配器模式\main.go
@@ -36,6 +36,7 @@ func (a Adapter) NewMethod() {
 	a.OldMethod()
 }
 
+// 适配器模式(Adapter Pattern)是作为两个不兼容接口之间的桥梁。适配器模式将一个类的接口转换为另一个类的接口，使得原本由于接口不兼容而不能一起工作的类可以一起工作。
 func main() {
 	oldInterface := OldImpl{}
 	a := Adapter{OldInterface: oldInterface}
